89987349c5c56a2b8c4ef683bed8b75a73a4fb16
Mike Doherty <doherty@cpan.org>
2010-10-18 14:43:27 +0000

    Import of DOHERTY/App-Nopaste-Service-Sprunge-0.001 from CPAN.
    gitpan-cpan-distribution: App-Nopaste-Service-Sprunge
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         DOHERTY/App-Nopaste-Service-Sprunge-0.001.tar.gz
    gitpan-cpan-author:       DOHERTY
    gitpan-cpan-maturity:     released

---
+++ lib/App/Nopaste/Service/Sprunge.pm
@@ -0,0 +1,84 @@
+use strict;
+use warnings;
+
+package App::Nopaste::Service::Sprunge;
+BEGIN {
+  $App::Nopaste::Service::Sprunge::VERSION = '0.001';
+}
+# ABSTRACT: Adds sprunge.us support to L<App::Nopaste>
+
+use base 'App::Nopaste::Service';
+
+sub available {
+    eval 'require WWW::Pastebin::Sprunge::Create; 1'
+}
+
+sub run {
+    my $self = shift;
+    my %args = @_;
+
+    require WWW::Pastebin::Sprunge::Create;
+
+    my $paster = WWW::Pastebin::Sprunge::Create->new();
+    my $ok = $paster->paste(
+        $args{'text'},
+        lang    => $args{'lang'},
+    );
+
+    return (0, $paster->error) unless $ok;
+    return (1, $paster->paste_uri);
+}
+
+
+1;
+
+
+
+=pod
+
+=head1 NAME
+
+App::Nopaste::Service::Sprunge - Adds sprunge.us support to L<App::Nopaste>
+
+=head1 VERSION
+
+version 0.001
+
+=head1 SEE ALSO
+
+L<WWW::Pastebin::Sprunge::Create>
+
+=head1 AVAILABILITY
+
+The latest version of this module is available from the Comprehensive Perl
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you, or see L<http://search.cpan.org/dist/App-Nopaste-Service-Sprunge/>.
+
+The development version lives at L<http://github.com/doherty/App-Nopaste-Service-Sprunge>
+and may be cloned from L<git://github.com/doherty/App-Nopaste-Service-Sprunge>.
+Instead of sending patches, please fork this project using the standard
+git and github infrastructure.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests through the web interface at
+L<http://github.com/doherty/App-Nopaste-Service-Sprunge/issues>.
+
+=head1 AUTHOR
+
+Mike Doherty <doherty@cs.dal.ca>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2100 by Mike Doherty.
+
+This is free software, licensed under:
+
+  The GNU General Public License, Version 3, June 2007
+
+=cut
+
+
+__END__
